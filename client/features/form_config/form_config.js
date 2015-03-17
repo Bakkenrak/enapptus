558fd7c2d42380bbed651d2bf3b3dd79d4ee4848
Lucas <lucas.berghaeuser@gmx.de>
2015-03-17 19:38:52 +0100

    added comments and "textfeld" option

--- client/features/form_config/form_config.js
+++ client/features/form_config/form_config.js
@@ -1,20 +1,52 @@
 app.controller('testCtrl', function($scope, apiFactory, $q, index){
-	$scope.type_options = [ 'Textzeile'];
-	$scope.selected_field = {};
-	angular.forEach(index.data, function(elm){
-		elm.rank = Number(elm.rank);
-	});
-	$scope.form_fields = index.data; 
+
+	/**************************************************************
+
+							Function Declarations
+
+
+	***************************************************************/
+
+
+
+	/**
+	 * workaround for parsing field object's rank from string to number when loading from backend. (needed for ng-repeat orderBy filter)
+	 * @param  {array} fieldarray array containing form field objects
+	 * @return {array}            array containing form field objects
+	 */
+	var convertStringToNumber = function(fieldarray){
+		angular.forEach(fieldarray, function(elm){
+			elm.rank = Number(elm.rank);
+		});
+		return fieldarray;
+	}
+
+
+
+	/**
+	 * scope function for copying field object into selected_field object for editing
+	 * @param  {object} field selected field
+	 * @return {undefined}       
+	 */
 	$scope.selectField = function(field){
 		$scope.new_field = undefined;
 		$scope.selected_field = angular.copy(field);
 	}
-
+	/**
+	 * scope function for saving a field object	 		
+	 * @param  {object} field field object
+	 * @return {undefined}       
+	 */
 	$scope.saveExistingField = function(field){
 		apiFactory.save(field).success(function(data){
 			console.log(data);
 		});
 	}
+	/**
+	 * scope function for deleting a form field object. Reloads View on success
+	 * @param  {object} field form field object
+	 * @return {undefined}  
+	 */
 	$scope.deleteFormField = function(field){
 		apiFactory.deleteElement(field).success(function(data, status){
 			if(status === 200){
@@ -24,14 +56,22 @@ app.controller('testCtrl', function($scope, apiFactory, $q, index){
 			}
 		})
 	};
-
+	/**
+	 * scope function for preparing a new field object 
+	 * @return {undefined}
+	 */
 	$scope.newField = function(){
 		$scope.selected_field = {};
 		$scope.new_field = {
 			rank: $scope.form_fields.length
 		};
 	}
-
+	/**
+	 * scope function for saving a new form field. If the new form field is not added at the end of the form 
+	 * but between already existing fields, then the following fields' rank is updated as well.  
+	 * @param  {object} field new field object
+	 * @return {undefined}
+	 */
 	$scope.saveNewField = function(field){
 		var promises = [];
 		field.fId = '0';
@@ -70,14 +110,14 @@ app.controller('testCtrl', function($scope, apiFactory, $q, index){
 		}
 	};
 
+	/**
+	 * reloads all form fields. Loaded fields are stored in scope variable "form_fields"
+	 * @return {undefined}
+	 */
 	var reloadAll = function(){
 		apiFactory.index().success(function(res, status){
 			if(status === 200){
-				console.log(res);
-				angular.forEach(index.data, function(elm){
-					elm.rank = Number(elm.rank);
-				});
-				$scope.form_fields = res;
+				$scope.form_fields = convertStringToNumber(res);
 			}else{
 				console.log('err when reloading', res);
 			}
@@ -85,37 +125,64 @@ app.controller('testCtrl', function($scope, apiFactory, $q, index){
 		});
 	}
 
-
+	/**
+	 * scope function for increasing the rank of an existing form field. -> field moves down. 
+	 * form field is saved after this as well as a possible affected field next to it. 
+	 * @param  {object} field form field object
+	 * @return {undefined} 
+	 */
 	$scope.moveDown = function(field){
 		field.rank++;
 		if($scope.selected_field.fId === field.fId){
 					$scope.selected_field.rank++;
 		}
+		apiFactory.save(field);
 		for(var i =0; i<$scope.form_fields.length; i++){
 			if(field.rank === $scope.form_fields[i].rank && field.fId !== $scope.form_fields[i].fId){
 				$scope.form_fields[i].rank--;
+				apiFactory.save($scope.form_fields[i]);
 				if($scope.selected_field.fId === $scope.form_fields[i].fId){
 					$scope.selected_field.rank--;
 				}
 			}
 		}
 	};
-
+	/**
+	 * scope function for decreasing the rank of an existing form field object. -> field moves up.
+	 * form field is saved after decreasing the rank as well as a possible affected neighbor. 
+	 * @param  {object} field field form field object
+	 * @return {undefined} 
+	 */
 	$scope.moveUp = function(field){
-		console.log('move up');
 		field.rank--;
+		apiFactory.save(field);
 		if($scope.selected_field.fId === field.fId){
 			$scope.selected_field.rank--;
 		}
 		for(var i =0; i<$scope.form_fields.length; i++){
 			if(field.rank === $scope.form_fields[i].rank && field.fId !== $scope.form_fields[i].fId){
-				console.log('change other', $scope.form_fields[i]);
 				$scope.form_fields[i].rank++;
+				apiFactory.save($scope.form_fields[i]);
 				if($scope.selected_field.fId === $scope.form_fields[i].fId){
 					$scope.selected_field.rank++;
 				}
 			}
 		}
 	};
+
+
+
+
+	/**************************************************************
+
+							Scope Init
+
+
+	***************************************************************/
+
+
+	$scope.type_options = [ 'Textzeile', 'Textfeld'];
+	$scope.selected_field = {};
+	$scope.form_fields = convertStringToNumber(index.data); 
 
 });
