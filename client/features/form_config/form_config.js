0c4972a595ab10100d98c35aba9b08dcd91b9d71
Lucas <lucas.berghaeuser@gmx.de>
2015-03-18 00:06:58 +0100

    refactored form configurator

--- client/features/form_config/form_config.js
+++ client/features/form_config/form_config.js
@@ -6,14 +6,6 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 
 
 	***************************************************************/
-	var toast = {
-		success : function(){
-			toaster.pop('success', 'Erfolg', 'Aktion ausgeführt!');
-		},
-		error: function(){
-			toaster.pop('error', 'Fehler', 'Aktion fehlgeschlagen');
-		}
-	};
 
 	/**
 	 * Option Object Constructor
@@ -44,18 +36,20 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 	 * scope function for initialising a new options array when changing the option type
 	 * @return {undefined} 
 	 */
-	$scope.changeType = function(){
-		$scope.selected_field.placeholder = '';
-		$scope.selected_field.option = [];
+	$scope.changeType = function(field){
+		field.placeholder = '';
+		field.option = [];
 	}
 	/**
-	 * scope function for copying field object into selected_field object for editing
+	 * scope function for selecting object for editing.
 	 * @param  {object} field selected field
 	 * @return {undefined}       
 	 */
 	$scope.selectField = function(field){
-		$scope.new_field = undefined;
-		$scope.selected_field = angular.copy(field);
+		angular.forEach($scope.form_fields, function(elm){
+			elm.isSelected = false;
+		});
+		field.isSelected = true;
 	}
 
 	/**
@@ -65,81 +59,54 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 	 */
 	$scope.saveField = function(field){
 		if(field.fId === 0){ // is new field
-			var promises = [];
-			field.rank = Number(field.rank);
-			var user_selected_rank = field.rank;
-			if(user_selected_rank === $scope.form_fields.length){
-				formApiFactory.save(field).success(
-					function(data, status){
-						if(status === 200){
-							$scope.form_fields.push(data);
-							toast.success();
-						}else{
-							toast.error();
-						}
-				});
-			}else{
-				angular.forEach($scope.form_fields, function(elm){
-					if(elm.rank>=user_selected_rank){
-						elm.rank++;
-						promises.push(formApiFactory.save(elm));
-					}
-				});
-				$q.all(promises).then(function(result){
-					formApiFactory.save(field).success(
-					function(data, status){
-						if(status===200){ 
-							toast.success();
-							reloadAll();
-						}else{
-							toast.error();
-						}
-					});
-				}, function(err){
+			formApiFactory.save(field).success(function(new_field, status){
+				if(status == 200){
+					toast.success();
+					replaceFieldByID(new_field,0);
+				}else{
 					toast.error();
-				});
-			}
+				}
+			});
 		}else{	// is existing field
 			formApiFactory.save(field).success(function(saved_field, status){
+				console.log('saved',saved_field);
 				if(status == 200){
 					replaceFieldByID(saved_field);
 					toast.success();
 				}else{
 					toast.error();
 				}
-				
 			});
 		}
-
 	}
 	/**
 	 * helper function for replacing a form field identified by id
 	 * @param  {object} field changed form field
+	 * @param {boolean} isNew indicator whether to look for new field (fId = 0)
 	 * @return {undefined}
 	 */
-	var replaceFieldByID = function(field){
-		var id = field.fId;
+	var replaceFieldByID = function(field, isNew){
+
+		var id = isNew ? 0 : field.fId;
 		for(var i = 0; i<$scope.form_fields.length; i++){
 			if($scope.form_fields[i].fId == id){
 				$scope.form_fields[i] = field; 
 				return undefined;
 			}
 		}
-	}
+	};
 	
 	/**
 	 * scope function for adding a new option (e.g. dropdown option) to a form field object's options property
 	 */
-	$scope.addOption = function(){
-		console.log(typeof $scope.selected_field.option);
-		var opt = new Option($scope.selected_field.new_option, 'option');
-		if(typeof $scope.selected_field.option === 'object'){
-			$scope.selected_field.option.push(opt);
+	$scope.addOption = function(field){
+		var opt = new Option(field.new_option, 'option');
+		if(typeof field.option === 'object'){
+			field.option.push(opt);
 		}else{
-			$scope.selected_field.option = [opt];
+			field.option = [opt];
 		}
-		$scope.selected_field.new_option = '';
-		console.log($scope.selected_field.option);
+		field.new_option = '';
 	}
 	/**
 	 * scope function for deleting a form field object. Following form field obejcts' ranks are decreased and saved. 
@@ -161,9 +128,6 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 				});
 				$q.all(promises).then(function(result){
 					reloadAll();
-					if(id == $scope.selected_field.fId){
-						$scope.selected_field = {};
-					}
 					toast.success();
 				}, function(err){
 					toast.error();
@@ -175,15 +139,29 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 	};
 
 	/**
-	 * scope function for preparing a new field object 
+	 * scope function for adding a new field object. Deletes every unsaved new field object. 
 	 * @return {undefined}
 	 */
 	$scope.newField = function(){
-		$scope.selected_field = {
+		var new_field = {
 			fId: 0,
+			type: 'Textzeile',
+			placeholder: 'Platzhalter',
+			option: [],
 			title: 'neues Feld',
 			rank: $scope.form_fields.length+1
 		};
+		if(typeof $scope.form_fields !== 'undefined'){
+			for(var i = 0, length = $scope.form_fields.length; i<length; i++){
+				if(Number($scope.form_fields[i].fId)===0){
+					$scope.form_fields.splice(i,1);
+					break;
+				}
+			}
+			$scope.form_fields.push(new_field);
+		}else{
+			$scope.form_fields = [new_field];
+		}
 	}
 
 	/**
@@ -209,17 +187,11 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 	 */
 	$scope.moveDown = function(field){
 		field.rank++;
-		if($scope.selected_field.fId === field.fId){
-					$scope.selected_field.rank++;
-		}
 		formApiFactory.save(field);
 		for(var i =0; i<$scope.form_fields.length; i++){
 			if(field.rank === $scope.form_fields[i].rank && field.fId !== $scope.form_fields[i].fId){
 				$scope.form_fields[i].rank--;
 				formApiFactory.save($scope.form_fields[i]);
-				if($scope.selected_field.fId === $scope.form_fields[i].fId){
-					$scope.selected_field.rank--;
-				}
 			}
 		}
 	};
@@ -232,16 +204,10 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 	$scope.moveUp = function(field){
 		field.rank--;
 		formApiFactory.save(field);
-		if($scope.selected_field.fId === field.fId){
-			$scope.selected_field.rank--;
-		}
 		for(var i =0; i<$scope.form_fields.length; i++){
 			if(field.rank === $scope.form_fields[i].rank && field.fId !== $scope.form_fields[i].fId){
 				$scope.form_fields[i].rank++;
 				formApiFactory.save($scope.form_fields[i]);
-				if($scope.selected_field.fId === $scope.form_fields[i].fId){
-					$scope.selected_field.rank++;
-				}
 			}
 		}
 	};
@@ -255,7 +221,15 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 
 
 	***************************************************************/
-
+	
+	var toast = {
+		success : function(){
+			toaster.pop('success', 'Erfolg', 'Aktion ausgeführt!');
+		},
+		error: function(){
+			toaster.pop('error', 'Fehler', 'Aktion fehlgeschlagen');
+		}
+	};
 
 	$scope.type_options = {
 		'Radiobutton' : {
@@ -280,7 +254,6 @@ app.controller('formConfigCtrl', function($scope, formApiFactory, $q, index, toa
 		}
 	};
 
-	$scope.selected_field = {};
 	$scope.form_fields = convertStringToNumber(index.data); 
 
 });
