674d0f91dad3ebe4d94cbd35755eb821cb85c0de
Lucas <lucas.berghaeuser@gmx.de>
2015-03-19 21:27:46 +0100

    added comments to myUser factory

--- client/factories/my_user_factory.js
+++ client/factories/my_user_factory.js
@@ -1,12 +1,27 @@
 app.factory('myUser', function(){
-	var this_user = {}
+	/** @type {Object} storage for current user object */
+	var this_user = {};
+	
 	return {
+		/**
+		 * setter for private current user object
+		 * @param {object} user current user
+		 */
 		setUser: function(user){
 			this_user = user;
 		},
+		/**
+		 * getter for private current user object
+		 * @return {Object} current user
+		 */
 		getUser: function(){
 			return this_user;
-		}, 
+		},
+
+		/**
+		 * function for getting admin state
+		 * @return {Boolean} boolean indicating admin status
+		 */
 		isAdmin: function(){
 			if(typeof this_user === 'object'){
 				return this_user.is_admin;
