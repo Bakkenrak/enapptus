f352e696abfb5acac272d19b094ead7ff58bdac9
Bakkenrak <christian.rest@hotmail.de>
2015-04-03 23:16:15 +0200

    User management

--- client/factories/user_api_factory.js
+++ client/factories/user_api_factory.js
@@ -1,51 +1,10 @@
 app.factory('userApiFactory', function($http){
 
-
-	/**
-	 * dummy list for testing
-	 * @type {Array}
-	 */
-	var users = [
-		{
-			id: 1,
-			username: 'lucas',
-			email: 'test@test',
-			is_admin: true,
-			is_active: true
-		},
-		{
-			id: 2,
-			username: 'tester1',
-			email: 'test@test',
-			is_admin: false,
-			is_active: false
-		},
-		{
-			id: 3,
-			username: 'tester2',
-			email: 'test@test',
-			is_admin: false,
-			is_active: true
-		},
-		{
-			id: 4,
-			username: 'tester3',
-			email: 'test@test',
-			is_admin: false,
-			is_active: true
-		}
-
-	];
-
-
-
-
-
 	return {
 		save : function(data){
 			return $http({
 				method:'POST',
-				url: '',
+				url: 'api/member/save',
 				data: data,
 				headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
 			});
@@ -53,18 +12,21 @@ app.factory('userApiFactory', function($http){
 		update : function(data){
 			return $http({
 				method:'POST',
-				url: '',
+				url: 'api/member/save',
 				data: data,
 				headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
 			});
 		}, 
 		index : function(){
-			return users;
+			return $http({
+				method:'GET',
+				url: 'api/member'
+			});
 		},
-		deleteElement : function(field){
+		deleteElement : function(member){
 			return $http({
 				method:'DELETE',
-				url: '',
+				url: 'api/member/'+member.mId,
 				headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
 			});
 		},
--- client/features/usermanagement/user.js
+++ client/features/usermanagement/user.js
@@ -8,6 +8,18 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 
 	***************************************************************/
 
+	/**
+	 * workaround for parsing member's admin status from string number to boolean when loading from backend.
+	 * @param  {array} membersarray array containing member objects
+	 * @return {array}            array containing member objects
+	 */
+	var convertNumberStringToBoolean = function(membersarray){
+		angular.forEach(membersarray, function(elm){
+			elm.admin = elm.admin === "1" ? true : false;
+		});
+		return membersarray;
+	}
+
 
 	/**
 	 * helper function for finding and deleting user object from user list
@@ -16,8 +28,15 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 	 */
 	var removeFromList = function(user){
 		for (var i = $scope.users.length - 1; i >= 0; i--) {
-				if(user.id === $scope.users[i].id){
-					$scope.users.splice(i, 1);
+				if(user.mId === $scope.users[i].mId){
+					userApiFactory.deleteElement(user).success(function(data, status){
+						if(status === 200){
+							$scope.users.splice(i, 1);
+							toaster.pop('success', 'Löschen', 'Mitglied '+ user.name +' gelöscht.');
+						}else{
+							toast.error();
+						}
+					})
 				}
 			};	
 	};
@@ -29,6 +48,14 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 	 */
 	$scope.saveUser = function(user){
 		console.log('save', user);
+		userApiFactory.save(user).success(function(data, status){
+						if(status === 200){
+							user.editMode = false;
+							user.mId = data.mId;
+						}else{
+							toast.error();
+						}
+					})
 		$rootScope.$broadcast('user.changed', user);
 	};
 
@@ -40,7 +67,33 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 	$scope.deleteUser = function(user){
 			myModal.setContent({'user':user});
 			modal.activate();	
-		};	
+		};
+
+
+	/**
+	 * scope function for adding a new field object. Deletes every unsaved new field object. 
+	 * @return {undefined}
+	 */
+	$scope.newMember = function(){
+		var new_member = {
+			mId: 0,
+			name: '',
+			password: '',
+			admin: false,
+			editMode: true
+		};
+		if(typeof $scope.users !== 'undefined'){
+			for(var i = 0, length = $scope.users.length; i<length; i++){
+				if(Number($scope.users[i].mId)===0){
+					$scope.users.splice(i,1);
+					break;
+				}
+			}
+			$scope.users.push(new_member);
+		}else{
+			$scope.users = [new_member];
+		}
+	}	
 
 
 	/** listener for user.deleted event. 
@@ -49,7 +102,6 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 	 */
 	$scope.$on('user.deleted', function(event, args){
 		removeFromList(args);
-		toaster.pop('success', 'Löschen', 'Benutzer'+ args.username +'gelöscht.');
 	});
 
 
@@ -60,7 +112,7 @@ app.controller('userManagementCtrl', function($scope, myModal, toaster, myUser, 
 
 	***************************************************************/
 
-	$scope.users = users;
+	$scope.users = convertNumberStringToBoolean(users.data);
 
 	/** @type {object} modal service for controlling the user modal */
 	var modal = myModal.getNewModal('./client/features/usermanagement/modal.html', 'userModalCtrl');
